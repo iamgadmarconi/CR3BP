95ab084ca6bc04243b2f96c3725a5f215cfbf9ff
iamgadmarconi <gadmarconi4@outlook.com>
2025-02-25 12:25:32 +0000

    Refactor CRTBP dynamics with modular energy and potential calculations

--- src/dynamics/crtbp.py
+++ src/dynamics/crtbp.py
@@ -3,26 +3,53 @@ import numpy as np
 import sympy as sp
 
 
-@numba.njit(fastmath=True, cache=True)
-def jacobi_integral(state, mu):
-    x, y, z, vx, vy, vz = state
+def hill_region(state, mu):
+    pass
 
+def _kinetic_energy(state):
+    x, y, z, vx, vy, vz = state
+    return 1 / 2 * (vx**2 + vy**2 + vz**2)
+
+def energy_integral(state, mu):
+    x, y, z, vx, vy, vz = state
+    U_eff = _effective_potential(state, mu)
+    return 1 / 2 * (vx**2 + vy**2 + vz**2) + U_eff
+
+def jacobi_constant(state, mu):
+    x, y, z, vx, vy, vz = state
+    U_eff = _effective_potential(state, mu)
+    return - (vx**2 + vy**2 + vz**2) - 2 * U_eff
+
+def _effective_potential(state, mu):
+    x, y, z, vx, vy, vz = state
+    U = _potential(state, mu)
+    U_eff = - 1 / 2 * (x**2 + y**2) + U
+    return U_eff
+
+def _potential(state, mu):
+    x, y, z, vx, vy, vz = state
     mu_1 = 1 - mu
     mu_2 = mu
-
-    r1 = np.sqrt((x + mu_2)**2 + y**2 + z**2)
-    r2 = np.sqrt((x - mu_1)**2 + y**2 + z**2)
-
+    r1 = _primary_distance(state, mu)
+    r2 = _secondary_distance(state, mu)
     U = - mu_1 / r1 - mu_2 / r2 - 1 / 2 * mu_1 * mu_2
+    return U
 
-    U_eff = - 1 / 2 * (x**2 + y**2) + U
+def _primary_distance(state, mu):
+    x, y, z, vx, vy, vz = state
+    mu_2 = mu
+    r1 = np.sqrt((x + mu_2)**2 + y**2 + z**2)
+    return r1
 
-    return - (vx**2 + vy**2 + vz**2) + 2 * U_eff
+def _secondary_distance(state, mu):
+    x, y, z, vx, vy, vz = state
+    mu_1 = 1 - mu
+    r2 = np.sqrt((x - mu_1)**2 + y**2 + z**2)
+    return r2
 
 def libration_points(mu):
     collinear = _collinear_points(mu)
     equilateral = _equilateral_points(mu)
-
     return collinear, equilateral
 
 def _equilateral_points(mu):
